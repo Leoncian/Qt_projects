b82de1938ee480a9db58db444a50c1102bc23c08
Leoncian <254910669@qq.com>
2025-10-21 20:38:52 +0800

    Initial commit: Qt project setup

---
+++ sensor/loginwindow.h
@@ -0,0 +1,33 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QSqlQuery>
+#include "db_manager.h"
+#include "mainwindow.h" // Add this line
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class LoginWindow; }
+QT_END_NAMESPACE
+
+class LoginWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    LoginWindow(QWidget *parent = nullptr);
+    ~LoginWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    int flag;
+    Ui::LoginWindow *ui;
+    void test_db();
+    MainWindow *sensorMainWindow; // Declare a pointer to MainWindow
+};
+#endif // LOGINWINDOW_H
